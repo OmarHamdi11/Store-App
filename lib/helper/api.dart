65a0915ba1005ae33d377411b502b1185a1daf2d
OmarHamd11 <omarellafy1@gmail.com>
2024-07-07 17:48:39 +0300

    post requist and add product service service

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -11,6 +13,31 @@ class Api {
     } else {
       throw Exception(
           'There is a problem with states code ${response.statusCode}');
+    }
+  }
+
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+      });
+    }
+
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          'There is a problem with states code ${response.statusCode} with body ${jsonDecode(response.body)}');
     }
   }
 }
